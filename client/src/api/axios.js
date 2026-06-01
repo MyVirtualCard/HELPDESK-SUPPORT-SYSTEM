357416aa113069c4d441175a83969a3573651be2
MyVirtualCard <ats.myvirtualcard@gmail.com>
2026-06-01 14:46:42 +0530

    wetwe

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://helpdesk-support-system-jdch.onrender.com/api",
 });
 
 export default API;
--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5001", {
+const socket = io("https://helpdesk-support-system-jdch.onrender.com", {
   transports: ["websocket"],
 });
 
